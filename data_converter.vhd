87bb3068847fd0cc0b1f8d77cd61eb9cf761a29d
kaliban8812 <begeshmot@gmail.com>
2023-05-11 12:52:13 +0200

    add two diff lenght
    add two diferent length for input and output signals

--- data_converter.vhd
+++ data_converter.vhd
@@ -4,40 +4,42 @@ use ieee.numeric_std.all;
 
 entity data_converter is
     generic (
-        DATA_LENGTH : integer := 10
+        DATA_LENGTH_IN  : integer := 10;
+        DATA_LENGTH_OUT : integer := 12
     );
     port
     (
-        clk_i  : in std_logic;
-        rst_i  : in std_logic;
+        clk_i : in std_logic;
+        rst_i : in std_logic;
 
         stb_i  : in std_logic;
-        data_i : in std_logic_vector(DATA_LENGTH - 1 downto 0);
+        data_i : in std_logic_vector(DATA_LENGTH_IN - 1 downto 0);
         stb_o  : out std_logic;
-        data_o : out std_logic_vector(DATA_LENGTH - 1 downto 0)
+        data_o : out std_logic_vector(DATA_LENGTH_OUT - 1 downto 0)
     );
 end entity data_converter;
 
 architecture data_converter_rtl of data_converter is
-
+    signal rev_data : std_logic_vector(data_i'range);
 begin
 
     CNV_PROC : process (clk_i, rst_i)
     begin
         if rst_i = '1' then
-            stb_o  <= '0';
-            data_o <= (others => '0');
+            stb_o    <= '0';
+            rev_data <= (others => '0');
         elsif rising_edge(clk_i) then
             if stb_i = '1' then
-                for i in 0 to DATA_LENGTH - 1 loop
-                    data_o(i) <= data_i(DATA_LENGTH - 1 - i);
+                for i in 0 to DATA_LENGTH_IN - 1 loop
+                    rev_data(i) <= data_i(DATA_LENGTH_IN - 1 - i);
                 end loop;
-                stb_o <= '1';
-            else
-                stb_o  <= '0';
-                data_o <= (others => '0');
+                stb_o      <= '1';
+            else stb_o <= '0';
+                rev_data   <= (others => '0');
             end if;
         end if;
     end process CNV_PROC;
+    
+    data_o <= ('1',rev_data,'1');
 
 end architecture data_converter_rtl;
--- dut_text_file.vhd
+++ dut_text_file.vhd
@@ -19,11 +19,12 @@ architecture dut_text_file_rtl of dut_text_file is
     signal rst_i     : std_logic := '1';
     constant CLK_PER : time      := 10 ns;
 
-    constant DATA_LENGTH : integer := 8;
-    signal stb_i         : std_logic; -- stb from file reading process, '1' when the vector from the file is ready.
-    signal stb_o         : std_logic; -- stb from dut, when the data is ready stb is '1';
-    signal data_file     : std_logic_vector(DATA_LENGTH - 1 downto 0); -- data from file
-    signal data_o        : std_logic_vector(DATA_LENGTH - 1 downto 0); -- data from dut;
+    constant LENGTH_IN  : integer := 8;
+    constant LENGTH_OUT : integer := 10;
+    signal stb_i        : std_logic;                                -- stb from file reading process, '1' when the vector from the file is ready.
+    signal stb_o        : std_logic;                                -- stb from dut, when the data is ready stb is '1';
+    signal data_file    : std_logic_vector(LENGTH_IN - 1 downto 0); -- data from file
+    signal data_o       : std_logic_vector(LENGTH_OUT - 1 downto 0); -- data from dut;
 
     constant FILE_NAME_DATA_IN  : string := "D:/Vova/Prog/projects/data_verification/test_data_i.txt";
     constant FILE_NAME_DATA_OUT : string := "D:/Vova/Prog/projects/data_verification/test_data_o.txt";
@@ -40,20 +41,22 @@ begin
         MP_start_reset(10 * CLK_PER, rst_i);
         MP_read_file_vector_synch(clk_i, FILE_NAME_DATA_IN, data_file, stb_i);
         wait for 100 * CLK_PER;
-        MP_compare_files(FILE_NAME_DATA_OUT, FILE_NAME_DATA_REF, DATA_LENGTH);
+        MP_compare_files(FILE_NAME_DATA_OUT, FILE_NAME_DATA_REF, LENGTH_OUT);
         MP_end_simulation (10 * CLK_PER);
     end process MAIN_PROC;
     -- write data to the output file, ckl and stb is needed
     WRITE_DATA_FROM_CONVERTER : process
     begin
         wait until stb_o = '1';
-        MP_write_file_vector(clk_i, stb_o, FILE_NAME_DATA_OUT, data_o, DATA_LENGTH);
+        MP_write_file_vector(clk_i, stb_o, FILE_NAME_DATA_OUT, data_o, LENGTH_OUT);
         wait;
     end process;
 
+    -- in this dut the in and out buses have the same length.
     dut : entity work.data_converter(data_converter_rtl)
         generic map(
-            DATA_LENGTH => DATA_LENGTH
+            DATA_LENGTH_IN => LENGTH_IN,
+            DATA_LENGTH_OUT => LENGTH_OUT
         )
         port map
         (
